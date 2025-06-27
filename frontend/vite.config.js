38acf86f6d13cdbefefbaa940e4246695bd8a6db
AllDevApp <duysonrm@outlook.com>
2025-06-27 13:56:36 +0700

    Delete frontend/vite.config.js

--- frontend/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  base: '/secure-notepad/',  // ✅ Important — this tells Vite to prefix all assets & API calls
-  plugins: [vue()]
-})
